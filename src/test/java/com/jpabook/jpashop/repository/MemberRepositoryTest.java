f3168307724eafda208f7d050b02dfc943c664fa
kimheejoo <h32j00@gmail.com>
2021-01-12 23:13:24 +0900

    day01

--- src/test/java/com/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/com/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +21,7 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     public void testMember() throws Exception{
         //given
         Member member = new Member();
@@ -30,6 +32,7 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
 }
