e8cbd937669b12b6d0f5aef0473b920d88ece943
kimheejoo <h32j00@gmail.com>
2021-01-12 22:39:23 +0900

    day01

---
+++ src/main/java/com/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,18 @@
+package com.jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setHello("hi");
+		String data = hello.getHello();
+		System.out.println(data);
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
