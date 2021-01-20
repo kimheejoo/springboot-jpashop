29c8d8947843d8d1304b8c63ce8816dad23df9cb
kimheejoo <h32j00@gmail.com>
2021-01-21 04:48:10 +0900

    view of member

--- src/main/java/com/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/com/jpabook/jpashop/controller/MemberController.java
@@ -7,10 +7,12 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +27,12 @@ public class MemberController {
     }
 
     @PostMapping("/members/new")
-    public String create(@Valid MemberForm form){
+    public String create(@Valid MemberForm form, BindingResult result){ // bindingresult: 오류가 있으면 튕겨버리는데 오류가 result에 담겨서 실행
+
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
         Member member = new Member();
         member.setName(form.getName());
@@ -33,5 +40,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
